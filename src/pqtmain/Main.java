eca0231efe436294de55f00d067d02a236e5e8db
Peyroux Florentin <peyroux@a14p20.fil.univ-lille1.fr>
2016-01-04 17:24:36 +0100

    Ajout de quelques éléments

--- src/pqtmain/Main.java
+++ src/pqtmain/Main.java
@@ -1,13 +1,49 @@
 package pqtmain;
 
-import gameframework.gui.GameStatusBar;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+
+import pqtwindows.GameCanvasSB;
+
+import gameframework.base.ObservableValue;
+import gameframework.drawing.*;
+import gameframework.game.*;
+import gameframework.gui.*;
+
+
 
 public class Main {
 	
 	public static void main(String[] args) {
-		GameStatusBar sb = new GameStatusBar();
+		//Settings.
+		GameConfiguration aGameConfiguration = new GameConfiguration(30,30,10,3);
+		GameData aGameData = new GameData(aGameConfiguration);
+		GameCanvasSB aGameCanvas = new GameCanvasSB();
+		String windowsName = "SpaceBattle2016ExtendedUltimateVersionDeluxePremium2";
 		
-		System.out.println("ok");
+		GameStatusBar aStuBar = new GameStatusBar();
+		GameStatusBarElement<Integer> aBarElement = new GameStatusBarElement<Integer>("life", new ObservableValue<Integer>(3));
+		GameStatusBarElement<String> aBarElement2 = new GameStatusBarElement<String>("azerty", new ObservableValue<String>("azerty"));
+		
+		BackgroundImage bg = new BackgroundImage("/resources/spaceBg.jpg", aGameCanvas);
+		
+		//Creation of the game window.
+		GameWindow aGameWindow = new GameWindow(windowsName, aGameCanvas, aGameConfiguration,aBarElement,aBarElement2);
+		Graphics tmp = aGameCanvas.getCanevasGraphic();
+		bg.draw(tmp);
+		aGameWindow.createGUI();
+
+		
+		//Change the value of the 2nd element
+		aBarElement2.getElement().setValue("test");
+		
 	}
 
 }
---
+++ src/pqtwindows/GameCanvasSB.java
@@ -0,0 +1,81 @@
+package pqtwindows;
+
+import java.awt.Canvas;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.event.KeyListener;
+
+import gameframework.drawing.GameCanvas;
+
+public class GameCanvasSB implements GameCanvas{
+
+	protected final Canvas canvas;
+
+	public GameCanvasSB() {
+		canvas = new Canvas();
+	}
+	
+	public Graphics getCanevasGraphic(){
+		return canvas.getGraphics();
+	}
+
+	public Image createBuffer() {
+		return canvas.createImage(canvas.getWidth(), canvas.getHeight());
+	}
+
+	public MediaTracker createMediaTracker() {
+		return new MediaTracker(canvas);
+	}
+
+	public void drawImage(Graphics graphics, Image image, int x, int y) {
+		if (x <= canvas.getWidth() && y <= canvas.getHeight())
+			graphics.drawImage(image, x, y, canvas);
+		else
+			graphics.drawImage(image, 0, 0, canvas);
+	}
+
+	public void drawFullSizeImage(Graphics graphics, Image image) {
+		graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(),
+				canvas);
+	}
+
+	public void drawFullSizeImage(Image image) {
+		canvas.getGraphics().drawImage(image, 0, 0, canvas.getWidth(),
+				canvas.getHeight(), canvas);
+	}
+
+	public void setBounds(int x, int y, int width, int height) {
+		canvas.setBounds(x, y, width, height);
+	}
+
+	public int getWidth() {
+		return canvas.getWidth();
+	}
+
+	public int getHeight() {
+		return canvas.getHeight();
+	}
+
+	public void setSize(int width, int height) {
+		canvas.setSize(width, height);
+	}
+
+	public void addTo(Frame frame) {
+		frame.add(canvas);
+	}
+
+	public void addKeyListener(KeyListener keyStr) {
+		canvas.addKeyListener(keyStr);
+	}
+
+	public void removeKeyListener(KeyListener keyStr) {
+		canvas.removeKeyListener(keyStr);
+	}
+
+	public KeyListener[] getKeyListeners() {
+		return canvas.getKeyListeners();
+	}
+
+}
