3174cf1c0648ac081dcd779f42077914b812af72
Peyroux Florentin <peyroux@a14p20.fil.univ-lille1.fr>
2016-01-04 15:55:16 +0100

    Premier commit

---
+++ src/pqtmain/Main.java
@@ -0,0 +1,13 @@
+package pqtmain;
+
+import gameframework.gui.GameStatusBar;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		GameStatusBar sb = new GameStatusBar();
+		
+		System.out.println("ok");
+	}
+
+}
