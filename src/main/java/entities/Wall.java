9eb8de89432227cc77b5e9109365cc761e47f126
Riverside14 <valentindelbeke@hotmail.fr>
2016-02-01 12:53:21 +0100

    Delete useless comments

--- src/main/java/entities/Wall.java
+++ src/main/java/entities/Wall.java
@@ -9,25 +9,16 @@ import gameframework.motion.blocking.MoveBlocker;
 
 public class Wall implements MoveBlocker, GameEntity {
 
-	/*
-	 * Attributes
-	 */
 	protected Point position;
    	protected int width;
    	protected int height;
        
-   	/*
-   	 * Constructors
-   	 */
 	public Wall(int x, int y, int width, int height) {
 		this.position = new Point(x, y);
 		this.width = width;
 		this.height = height;
 	}
 
-	/*
-	 * Methods
-	 */
 	public Rectangle getBoundingBox() {
 		return new Rectangle(position.x, position.y, width , height);
 	}
